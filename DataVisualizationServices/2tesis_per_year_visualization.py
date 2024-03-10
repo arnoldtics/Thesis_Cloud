1d4067c5352dfab68aa876c60d61179cd73ad1ea
Arnoldo Sánchez <arfersan20@gmail.com>
2024-03-09 20:08:48 -0600

    Updating script for data visualization

--- DataVisualizationServices/2tesis_per_year_visualization.py
+++ DataVisualizationServices/2tesis_per_year_visualization.py
@@ -1,19 +1,28 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from collections import Counter
 
-PATH = "../DataCleaning/"
+PATH, frequency = "../DataAcquisition/Title-Author/", {}
 
-df = pd.read_csv(PATH+"Name_LastName_Title_Year.csv")
+def counting(filename):
+    with open(PATH + filename, "r", encoding="latin1") as file:
+        lines = file.readlines()
+        for line in lines:
+            if not line.startswith("https://tesiunam.dgb.unam.mx"):
+                year, line = "", line.strip()
+                for character in reversed(line):
+                    if character.isnumeric(): year = character + year
+                    if len(year) == 4: break
+                try: frequency[int(year)] += 1
+                except: frequency[int(year)] = 1
 
-frequency = Counter(df["Year"])
-for year in range(1840, 2025):
-    if year not in frequency: frequency[year] = 0
-frequency = sorted([item for item in frequency.items()], key=lambda x: x[0])
+counting("1800-1899_thesis-link.txt")
+counting("1900-1919_thesis-link.txt")
 
-years = np.array([year for year, freq in frequency])
-freq = np.array([freq for year, freq in frequency])
+for year in range(1920, 2025):
+    with open(PATH + str(year) + "_thesis-link.txt", "r", encoding="latin1") as file: frequency[year] = len(file.readlines()) // 2
+
+frequency = sorted([item for item in frequency.items()])
+years, freq = np.array([year for year, freq in frequency]), np.array([freq for year, freq in frequency])
 
 fig, ax = plt.subplots(figsize=(12,7))
 ax.set_title("Plot of years vs bachelor's theses published", fontsize=18)
