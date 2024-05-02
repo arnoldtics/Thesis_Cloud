516251b61a65ae00bde7cab813167c784e91d912
Arnoldo Sánchez <arfersan20@gmail.com>
2024-05-02 08:02:29 -0600

    Script for data preprocessing for deep learning model

---
+++ DeepLearningModel/3DataPreprocessing.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import spacy
+
+PATH = "../DataCleaning/"
+
+df = pd.read_csv("Author_Title_Link_Year.zip")
+nlp = spacy.load("es_core_news_sm")
+
+def preprocess_text(text):
+    doc = nlp(text)
+    tokens = [token.lemma_ for token in doc if not token.is_stop and token.is_alpha]
+    return tokens
+
+df["Vector"] = df["Title"].apply(preprocess_text)
+
+df.to_csv("PreprocessTesis.csv")
