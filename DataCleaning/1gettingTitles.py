a96e73b3b185c09728db992e665f6c9cca818029
unknown <arfersan20@gmail.com>
2025-05-10 19:07:14 -0600

    Improving titles cleaning

--- DataCleaning/1gettingTitles.py
+++ DataCleaning/1gettingTitles.py
@@ -1,18 +1,17 @@
-output = open("Titles.txt", "w", encoding="utf-8")
 PATH = "../DataAcquisition/Title-Author/"
 
-def writeTitles(input):
-    file = open(input, "r", encoding="utf-8")
-    lines = file.readlines()
-    if input == PATH + "1800-1899.txt" or input == PATH + "1900-1919.txt": lines.reverse()
-    for line in lines: 
-        i = line.find("sustentante") + 11
-        output.write(line[i:-9].strip() + "\n")
-    file.close()
+with open("Titles.txt", "w", encoding="utf8") as output:
+    def writeTitles(input):
+        with open(input, "r", encoding="utf8") as file:
+            lines = file.readlines()
+            for line in lines:
+                i = line.find("sustentante") + 11
+                title = line[i:].strip().split()
+                title = " ".join(title[:-1])
+                if title[-1] == "/": title = title[:-1]
+                output.write(title + "\n")
 
-writeTitles(PATH + "1800-1899.txt")
-writeTitles(PATH + "1900-1919.txt")
+    writeTitles(PATH + "1800-1899.txt")
+    writeTitles(PATH + "1900-1919.txt")
+    for year in range(1920, 2025): writeTitles(PATH + str(year) + ".txt")
 
-for year in range(1920, 2025): writeTitles(PATH + str(year) + ".txt")
-
-output.close()
